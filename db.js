10f973bfe41e365a58eece0d9cdf84bc8143f0e3
temusu <temusgaming@gmail.com>
2018-01-26 21:32:01 +0900

    Initial commit

---
+++ db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  clientId: '406372548322787328',
+  clientSecret: '--l5C156oLQoc15Zp4Dx204uvXT6gt5j',
+  token: 'NDA2MzcyNTQ4MzIyNzg3MzI4.DUx_6Q.HlghbQsNwMv3eBw8bhGWSsAzUBc',
+  prefix: '!',
+  musicChannel: 'music'
+}
