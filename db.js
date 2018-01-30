0514f31bbf90ab647b6f84b5a63064b228f9b7bd
temusu <temusgaming@gmail.com>
2018-01-30 21:14:56 +0900

    check herkou comp

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ module.exports = {
   clientId: '406372548322787328',
   clientSecret: '--l5C156oLQoc15Zp4Dx204uvXT6gt5j',
   token: 'NDA2MzcyNTQ4MzIyNzg3MzI4.DUx_6Q.HlghbQsNwMv3eBw8bhGWSsAzUBc',
+  gglApiKey: 'AIzaSyCgDLfGxXMNwIARoceQ3lcx3zKpx4CNZI8',
   prefix: '!',
   musicChannel: 'music'
 }
--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const yt = require('ytdl-core')
 
 const db = require('./db.js')
 
-
-
-
 //
 // DISCORD
 client.on('ready', () => {
@@ -17,45 +14,38 @@ client.on('message', msg => {
   let command
   let args // TODO
 
-  // no reply if msg author is bot
   if (msg.author.bot) return;
+  if (!msg.content.startsWith(db.prefix)) return;
 
-  if (msg.channel.name === db.musicChannel) {
-    command = msg.content.split(' ')[0]
-
-  } else {
-    // no reply if starts not with prefix
-    if (!msg.content.startsWith(db.prefix)) return;
-
-    command = msg.content.split(' ')[0].slice(db.prefix.length)
-    console.log(command)
-
-    if (command === "play") {
-      const voiceChannel = msg.member.voiceChannel;
-      if (!voiceChannel){
-        return msg.channel.sendMessage(":x: You must be in a voice channel first!");
-      }
-      voiceChannel.join()
-      .then(connection => {
-        console.log('1')
-        let stream = yt('https://www.youtube.com/watch?v=DyhNyMLeB78', {audioonly: true});
-        console.log('2')
-        yt.getInfo('https://www.youtube.com/watch?v=DyhNyMLeB78', function(err, info) {
-          console.log('3')
-        const title = info.title
-        console.log(`${msg.author.username}, Queued the song '${title}.'`)
-        msg.channel.sendMessage(`Now playing \`${title}\``)
-        })
-        const dispatcher = connection.playStream(stream);
-        dispatcher.on('end', () => {
-           voiceChannel.leave();
-         }).catch(e =>{
-           console.error(e);
-         });
-      })
-    }
+  command = msg.content.split(' ')[0].slice(db.prefix.length)
+  args = msg.content.split(' ').splice(1)
+  
+  if (command === "ping") {
+    msg.channel.sendMessage(`\`${Date.now() - msg.createdTimestamp} ms\``);
   }
+  if (command === "play"){
+    const voiceChannel = msg.member.voiceChannel;
+    if (!voiceChannel){
+      return msg.channel.sendMessage(":x: You must be in a voice channel first!");
+    }
+    voiceChannel.join()
+    .then(connection => {
+      let stream = yt(args.join(" "), {audioonly: true});
+      yt.getInfo(args.join(" "), function(err, info) {
+      const title = info.title
+      console.log(`${msg.author.username}, Queued the song '${title}.'`)
+      msg.channel.sendMessage(`Now playing \`${title}\``)
+      })
+      const dispatcher = connection.playStream(stream);
+      dispatcher.on('end', () => {
+         voiceChannel.leave();
+       }).catch(e =>{
+         console.error(e);
+       });
+    })
+  }
+  
 })
 
-client.login(db.token)
+client.login(process.env.TOKEN || db.token)
 
